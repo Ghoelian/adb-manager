2c34045ffdaa70647c82aa8cbc8bc1f84f9272dd
Julian Vos <post@julianvos.nl>
2024-01-28 12:23:43 +0100

    chore: add repository setting placeholder

--- lib/src/settings/presentation/settings_screen.dart
+++ lib/src/settings/presentation/settings_screen.dart
@@ -11,13 +11,23 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return ListView(
+      // TODO: temporary, should make a central settings store and dynamically create these tiles at some point
       children: const [
+        Tooltip(
+          message: "Repository where scripts will be pulled from.",
+          child: ListTile(
+            title: Text("Script repository"),
+            subtitle:
+                Text("https://github.com/Ghoelian/adb-manager-scripts.git"),
+          ),
+        ),
         Tooltip(
           message: "How often the program scans for new devices.",
           child: ListTile(
-          title: Text("Scan interval"),
-          subtitle: Text("10 seconds"),
-        ),),
+            title: Text("Scan interval"),
+            subtitle: Text("10 seconds"),
+          ),
+        ),
         Tooltip(
           message:
               "If a device gets disconnected and re-connected within this time, scripts will not be re-run.",
