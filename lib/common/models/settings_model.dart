e93261243c307872c49d0512a161ea447c796b57
Julian Vos <post@julianvos.nl>
2024-01-28 13:38:17 +0100

    feat: save settings to shared prefs

--- lib/common/models/settings_model.dart
+++ lib/common/models/settings_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Setting {
   final String key;
@@ -21,12 +22,49 @@ class SettingsModel extends ChangeNotifier {
   final rerunTimeout =
       Setting(key: "reconnect_timeout", defaultValue: 10 * 1000);
 
-  void updateSetting(String key, dynamic value) {
+  bool loading = true;
+
+  SettingsModel() {
+    populateSettings();
+  }
+
+  Future<void> populateSettings() async {
+    loading = true;
+    notifyListeners();
+
+    final prefs = await SharedPreferences.getInstance();
+
+    final repoValue = prefs.getString("script_repository");
+    final reloadValue = prefs.getInt("reload_interval_millis");
+    final rerunValue = prefs.getInt("reconnect_timeout");
+
+    if (repoValue != null) {
+      scriptRepository.value = repoValue;
+    }
+
+    if (reloadValue != null) {
+      reloadInterval.value = reloadValue;
+    }
+
+    if (rerunValue != null) {
+      rerunTimeout.value = rerunValue;
+    }
+
+    loading = false;
+    notifyListeners();
+  }
+
+  // TODO: fix this mess holy shit this is unreadable
+  Future<void> updateSetting(String key, dynamic value) async {
+    final prefs = await SharedPreferences.getInstance();
+
     switch (key) {
       case "script_repository":
         if (value == null) {
           scriptRepository.value = null;
           notifyListeners();
+
+          await prefs.remove("script_repository");
           return;
         }
 
@@ -35,11 +73,15 @@ class SettingsModel extends ChangeNotifier {
         if (parsedValue == null) return;
 
         scriptRepository.value = parsedValue;
+        await prefs.setString("script_repository", parsedValue.toString());
+
         break;
       case "reload_interval_millis":
         if (value == null) {
           reloadInterval.value = null;
           notifyListeners();
+
+          await prefs.remove("reload_interval_millis");
           return;
         }
 
@@ -49,11 +91,15 @@ class SettingsModel extends ChangeNotifier {
         if (parsedValue == null) return;
 
         reloadInterval.value = parsedValue;
+        await prefs.setInt("reload_interval_millis", parsedValue);
+
         break;
       case "reconnect_timeout":
         if (value == null) {
           rerunTimeout.value = null;
           notifyListeners();
+
+          await prefs.remove("reconnect_timeout");
           return;
         }
 
@@ -63,6 +109,9 @@ class SettingsModel extends ChangeNotifier {
         if (parsedValue == null) return;
 
         rerunTimeout.value = parsedValue;
+        await prefs.setInt("reconnect_timeout", parsedValue);
+
+        break;
       default:
         throw UnsupportedError("Unknown setting");
     }
